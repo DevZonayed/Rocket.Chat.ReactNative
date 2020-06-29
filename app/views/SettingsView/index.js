DevZonayed/Rocket.Chat.ReactNative

commit 79a56427a1096e6e9acd5e0c9603c24cc24f86fa

[NEW] Omnichannel Status Toggle (#2217)
Co-authored-by: Diego Mello <diegolmello@gmail.com>

app/views/SettingsView/index.js
@@ -70,8 +70,18 @@ class SettingsView extends React.Component {
 		isMasterDetail: PropTypes.bool,
 		logout: PropTypes.func.isRequired,
 		selectServerRequest: PropTypes.func,
-		token: PropTypes.string,
+		user: PropTypes.shape({
+			roles: PropTypes.array,
+			statusLivechat: PropTypes.string
+		}),
 		appStart: PropTypes.func
+	}
+
+	get showLivechat() {
+		const { user } = this.props;
+		const { roles } = user;
+
+		return roles.includes('livechat-agent');
 	}
 
 	handleLogout = () => {
@@ -111,6 +121,14 @@ class SettingsView extends React.Component {
 			loggerConfig.clearBeforeSendCallbacks();
 		} else {
 			loggerConfig.registerBeforeSendCallback(() => false);
+		}
+	}
+
+	toggleLivechat = async() => {
+		try {
+			await RocketChat.changeLivechatStatus();
+		} catch {
+			// Do nothing
 		}
 	}
 
@@ -168,6 +186,18 @@ class SettingsView extends React.Component {
 				value={allowCrashReport}
 				trackColor={SWITCH_TRACK_COLOR}
 				onValueChange={this.toggleCrashReport}
+			/>
+		);
+	}
+
+	renderLivechatSwitch = () => {
+		const { user } = this.props;
+		const { statusLivechat } = user;
+		return (
+			<Switch
+				value={statusLivechat === 'available'}
+				trackColor={SWITCH_TRACK_COLOR}
+				onValueChange={this.toggleLivechat}
 			/>
 		);
 	}
@@ -292,6 +322,18 @@ class SettingsView extends React.Component {
 
 					<SectionSeparator theme={theme} />
 
+					{this.showLivechat ? (
+						<>
+							<ListItem
+								title={I18n.t('Omnichannel')}
+								testID='settings-view-livechat'
+								right={() => this.renderLivechatSwitch()}
+								theme={theme}
+							/>
+							<SectionSeparator theme={theme} />
+						</>
+					) : null}
+
 					<ListItem
 						title={I18n.t('Send_crash_report')}
 						testID='settings-view-crash-report'
@@ -331,7 +373,7 @@ class SettingsView extends React.Component {
 
 const mapStateToProps = state => ({
 	server: state.server,
-	token: getUserSelector(state).token,
+	user: getUserSelector(state),
 	allowCrashReport: state.crashReport.allowCrashReport,
 	isMasterDetail: state.app.isMasterDetail
 });

app/i18n/locales/en.js
@@ -334,6 +334,7 @@ export default {
 	No_available_agents_to_transfer: 'No available agents to transfer',
 	Offline: 'Offline',
 	Oops: 'Oops!',
+	Omnichannel: 'Omnichannel',
 	Onboarding_description: 'A workspace is your team or organization’s space to collaborate. Ask the workspace admin for address to join or create one for your team.',
 	Onboarding_join_workspace: 'Join a workspace',
 	Onboarding_subtitle: 'Beyond Team Collaboration',

app/lib/methods/subscriptions/rooms.js
@@ -14,6 +14,7 @@ import buildMessage from '../helpers/buildMessage';
 import RocketChat from '../../rocketchat';
 import EventEmitter from '../../../utils/events';
 import { removedRoom } from '../../../actions/room';
+import { setUser } from '../../../actions/login';
 import { INAPP_NOTIFICATION_EMITTER } from '../../../containers/InAppNotification';
 
 const removeListener = listener => listener.stop();
@@ -241,6 +242,10 @@ export default function subscribeRooms() {
 		}
 		const [type, data] = ddpMessage.fields.args;
 		const [, ev] = ddpMessage.fields.eventName.split('/');
+		if (/userData/.test(ev)) {
+			const [{ diff }] = ddpMessage.fields.args;
+			store.dispatch(setUser({ statusLivechat: diff?.statusLivechat }));
+		}
 		if (/subscriptions/.test(ev)) {
 			if (type === 'removed') {
 				try {

app/lib/rocketchat.js
@@ -401,6 +401,7 @@ const RocketChat = {
 			status: result.me.status,
 			statusText: result.me.statusText,
 			customFields: result.me.customFields,
+			statusLivechat: result.me.statusLivechat,
 			emails: result.me.emails,
 			roles: result.me.roles
 		};
@@ -808,6 +809,10 @@ const RocketChat = {
 	getCustomFields() {
 		// RC 2.2.0
 		return this.sdk.get('livechat/custom-fields');
+	},
+	changeLivechatStatus() {
+		// RC 0.26.0
+		return this.methodCall('livechat:changeLivechatStatus');
 	},
 
 	getUidDirectMessage(room) {

app/sagas/room.js
@@ -74,12 +74,12 @@ const handleDeleteRoom = function* handleDeleteRoom({ rid, t }) {
 };
 
 const handleCloseRoom = function* handleCloseRoom({ rid }) {
+	const isMasterDetail = yield select(state => state.app.isMasterDetail);
 	const requestComment = yield select(state => state.settings.Livechat_request_comment_when_closing_conversation);
 
 	const closeRoom = async(comment = '') => {
 		try {
 			await RocketChat.closeLivechat(rid, comment);
-			const isMasterDetail = await select(state => state.app.isMasterDetail);
 			if (isMasterDetail) {
 				Navigation.navigate('DrawerNavigator');
 			} else {

patches/@rocket.chat+sdk+1.0.0-dj.15.patch
@@ -81,3 +81,17 @@ index 17c2c2b..cb094e8 100644
  	/** Check result data for success, allowing override to ignore some errors */
    success (result: any, ignore?: RegExp) {
      return (
+diff --git a/node_modules/@rocket.chat/sdk/lib/drivers/ddp.ts b/node_modules/@rocket.chat/sdk/lib/drivers/ddp.ts
+index 247ce8b..2687abc 100644
+--- a/node_modules/@rocket.chat/sdk/lib/drivers/ddp.ts
++++ b/node_modules/@rocket.chat/sdk/lib/drivers/ddp.ts
+@@ -538,7 +538,8 @@ export class DDPDriver extends EventEmitter implements ISocket, IDriver {
+       'notification',
+       'rooms-changed',
+       'subscriptions-changed',
+-      'uiInteraction'
++      'uiInteraction',
++      'userData'
+     ].map(event => this.subscribe(topic, `${this.userId}/${event}`, false)))
+   }
+ 

commit 1d797d7b74096117c30a2cd5ba0ed12d74835a78

[REGRESSION] Logout failing after #2217 (#2222)

app/views/SettingsView/index.js
@@ -81,7 +81,7 @@ class SettingsView extends React.Component {
 		const { user } = this.props;
 		const { roles } = user;
 
-		return roles.includes('livechat-agent');
+		return roles?.includes('livechat-agent');
 	}
 
 	handleLogout = () => {